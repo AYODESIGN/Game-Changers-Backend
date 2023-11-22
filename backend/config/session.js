b2f47a6b6d3e04f8baec922d851a6b71b37bc5ec
AYODESIGN <104289006+AYODESIGN@users.noreply.github.com>
2023-11-22 15:21:11 +0100

    Update session.js

--- backend/config/session.js
+++ backend/config/session.js
@@ -4,8 +4,13 @@ const session = require('express-session');
 const sessionConfig = {
   secret: 'ayoub92', // Change to a secure secret key
   resave: false,
-  saveUninitialized: true,
-  // You can customize other session options as needed
+  saveUninitialized: false,
+    store: store,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    }
 };
+
+
 
 module.exports = sessionConfig;
