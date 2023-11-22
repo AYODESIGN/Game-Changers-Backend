38d210bea9d02e63b5b180cb52c68c55d2469ae2
AYODESIGN <104289006+AYODESIGN@users.noreply.github.com>
2023-11-22 15:27:28 +0100

    Update session.js

--- backend/config/session.js
+++ backend/config/session.js
@@ -1,16 +1,23 @@
+
+module.exports = sessionConfig;
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+// Create a new instance of MongoDBStore
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URL, // Replace with your MongoDB connection URI
+  collection: 'sessions',
+});
 
 // Configure your session settings
 const sessionConfig = {
   secret: 'ayoub92', // Change to a secure secret key
   resave: false,
   saveUninitialized: false,
-    store: store,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24, // 1 day
-    }
+  store: store,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
+  },
 };
-
-
 
 module.exports = sessionConfig;
