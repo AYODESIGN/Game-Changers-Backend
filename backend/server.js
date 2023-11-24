90e76e1298ef629e2d72d09a4b865dd45520059e
AYODESIGN <104289006+AYODESIGN@users.noreply.github.com>
2023-11-24 21:54:40 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 
 // import app express
 const app = require("./app")
+const port = process.env.PORT || 5000;
 
-//   http://localhost:4000
-app.listen(4000, () => {
-  console.log('Server is running on port 4000');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
+
+
